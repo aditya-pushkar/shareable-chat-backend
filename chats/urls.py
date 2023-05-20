d14007004b1d34c4d20b3e99b42be31df3ef5a36
aditya-pushkar <adityapushkar850@gmail.com>
2023-05-21 03:06:29 +0530

    slash/ --removed from URL

--- chats/urls.py
+++ chats/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('detail/<str:chat_id>/', chat_detail),
     path('create/', create_chat),
     path('update/', update_chat),
-    path('share/', share_chat_to_public),
+    path('share', share_chat_to_public),
     path('read/<str:chat_id>/', read_public_chat),
     path('fork/', fork_chat),
 ]
