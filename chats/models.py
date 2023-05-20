cbffd3d5a766bcdd2b971697d3190a734eddd59f
aditya-pushkar <adityapushkar850@gmail.com>
2023-05-20 17:56:29 +0530

    default chats --added

--- chats/models.py
+++ chats/models.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User
 
 import uuid
 
+def get_default_chat():
+    return [{"role": "system", "content": "You are a good assistant. Return all the result in the markdown."}]
 
 class Chat(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default="deleted user", related_name="chats")
     title = models.CharField(max_length=500, null=True, blank=True)
-    chats = models.JSONField(null=True, blank=True)
+    chats = models.JSONField(null=True, blank=True, default=get_default_chat)
     is_public = models.BooleanField(default=False)
     is_forked = models.BooleanField(default=False, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
